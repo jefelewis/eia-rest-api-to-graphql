3f05a25e5cef1e1aa4c5cf1e86cc72284e0060ca
Jeff Lewis <jeffylew77@gmail.com>
2018-10-29 16:08:19 -0700

    [Dev] Adding Resolvers

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,16 +1,42 @@
 // Imports: Dependencies
 import axios from 'axios';
 
+// Import: API Key
+import eaiAPIKey from '../../config/config';
+
 // GraphQL: Resolvers
 const RESOLVERS = {
   Query: {
 
-    // 
-    testQuery: (parent, args) => {
-      return axios.get(`http://api.eia.gov/`)
+    // CONSUMPTION FOR ELECTRICITY GENERATION
+    // Coal (Thousand Tons)
+    getCoalConsumptionForElectricity: (parent, args) => {
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.COW-US-99.M`)
         .then(response => response.data)
         .catch(error => console.log(error));
     },
+
+    // Natural Gas (Thousand MCF)
+    getNaturalGasConsumptionForElectricity: (parent, args) => {
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.NG-US-99.M`)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+    },
+    
+    // Petroleum Liquids (Thousand Barrels)
+    getPetroleumLiquidsConsumptionForElectricity: (parent, args) => {
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.PEL-US-99.M`)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+    },
+
+    // Petroleum Coke (Thousand Tons)
+    getPetroleumCokeConsumptionForElectricity: (parent, args) => {
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.PC-US-99.M`)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+    },
+
   },
 };
 
