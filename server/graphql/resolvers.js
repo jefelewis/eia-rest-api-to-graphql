04cccd2f5b4650f12fedf59affb06c7f9ebf201b
Jeff Lewis <jeffylew77@gmail.com>
2018-10-29 16:33:20 -0700

    [Dev] Adding Resolvers

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -8,35 +8,72 @@ import eaiAPIKey from '../../config/config';
 const RESOLVERS = {
   Query: {
 
-    // CONSUMPTION FOR ELECTRICITY GENERATION
+    // GENERATION OF ELECTRICITY
+    // Annual Net Generation (United States)
+    getAnnualNetGenerationOfElectricity: (parent, args) => {
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.GEN.ALL-US-99.A`)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+    },
+
+    // Annual Net Generation (By State)
+    getAnnualNetGenerationOfElectricityByState: (parent, args) => {
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.GEN.ALL-US-99.A`)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+    },
+
+    // CONSUMPTION OF ELECTRICITY GENERATION
     // Coal (Thousand Tons)
     getCoalConsumptionForElectricity: (parent, args) => {
-      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.COW-US-99.M`)
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.COW-US-99.A`)
         .then(response => response.data)
         .catch(error => console.log(error));
     },
 
     // Natural Gas (Thousand MCF)
     getNaturalGasConsumptionForElectricity: (parent, args) => {
-      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.NG-US-99.M`)
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.NG-US-99.A`)
         .then(response => response.data)
         .catch(error => console.log(error));
     },
     
     // Petroleum Liquids (Thousand Barrels)
     getPetroleumLiquidsConsumptionForElectricity: (parent, args) => {
-      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.PEL-US-99.M`)
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.PEL-US-99.A`)
         .then(response => response.data)
         .catch(error => console.log(error));
     },
 
     // Petroleum Coke (Thousand Tons)
     getPetroleumCokeConsumptionForElectricity: (parent, args) => {
-      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.PC-US-99.M`)
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.CONS_EG.PC-US-99.A`)
         .then(response => response.data)
         .catch(error => console.log(error));
     },
 
+    // COST OF ELECTRICITY
+    // Average Retail Price For Electricity
+    getAverageRetailPriceForElectricity: (parent, args) => {
+      return axios.get(`http://api.eia.gov/series/?api_key=${eaiAPIKey}&series_id=ELEC.PRICE.US-ALL.A`)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+    },
+
+    // CARBON DIOXIDE EMISSIONS
+    // Get Carbon Dioxide Emissions
+    getCarbonDioxideEmissions: (parent, args) => {
+      return axios.get(`http://api.eia.gov/category/?api_key=${eaiAPIKey}&category_id=2251604`)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+    },
+
+    // Get Carbon Dioxide Emissions By State
+    getCarbonDioxideEmissionsByState: (parent, args) => {
+      return axios.get(`http://api.eia.gov/category/?api_key=${eaiAPIKey}&category_id=2251670`)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+    },
   },
 };
 
