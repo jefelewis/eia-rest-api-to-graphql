664b879e8847b4397256a1e1a7235b940a237c83
Jeff Lewis <jeffylew77@gmail.com>
2018-10-30 12:13:18 -0700

    [Fix] Fixing import issue

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Imports: API Key
-import eaiAPIKey from '../../config/config';
+import { eaiAPIKey } from '../../config/config';
 
 // States
 let states = [ "AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"];
